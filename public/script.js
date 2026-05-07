f58dd23ffd7db70bf25142162b87b680c65e5804
Tarunsolanki <solankit950@gmail.com>
2026-05-07 20:04:07 +0530

    fix API hostname detection

--- public/script.js
+++ public/script.js
@@ -3,7 +3,8 @@ window.allPasswords = [];
 
 // 🌐 API
 const API_URL =
-  window.location.hostname === "localhost"
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
     : "https://securevault-backend-xpsb.onrender.com";
 
