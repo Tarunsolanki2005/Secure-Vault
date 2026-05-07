b50baaaaf4adc226eced92f31c3c81f55dac8012
Tarunsolanki <solankit950@gmail.com>
2026-05-07 20:00:47 +0530

    fix cors

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 //routes
 app.use("/api/auth", authRoutes);
